e28f3c6a4bcd0e712f246ab2d33a5964c0aab5d7
zablockikrzysztof13-code <zablockikrzysztof13@gmail.com>
2026-09-26 08:25:55 +0200

    Add Android APK wrapper

---
+++ android/app/build.gradle.kts
@@ -0,0 +1,12 @@
+plugins { id("com.android.application") }
+android {
+    namespace = "pl.fmoja.app"
+    compileSdk = 35
+    defaultConfig {
+        applicationId = "pl.fmoja.app"
+        minSdk = 24
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0"
+    }
+}
